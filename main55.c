de2bcf089d0ad4701319ed0749432d3b588e11b8
seyoungyook <xqd1219@naver.com>
2019-09-26 16:12:19 +0900

    project0926

---
+++ main55.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("-1L < IU : 5d\n", -1L < IU);
+	
+	return 0;
+}
